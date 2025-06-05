53f7bcdf44942114964b9629284af6cd016aa1e5
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-06-05 13:26:08 +0800

    Update Auras.lua

--- Interface/AddOns/NDui/Modules/Auras/Auras.lua
+++ Interface/AddOns/NDui/Modules/Auras/Auras.lua
@@ -321,11 +321,13 @@ function A:CreateAuraIcon(button)
 
 	button.count = button:CreateFontString(nil, "ARTWORK")
 	button.count:SetPoint("TOPRIGHT", -1, -3)
-	B.SetFontSize(button.count, fontSize)
+	button.count:SetFont("Interface\\AddOns\\NDui\\Media\\ROADWAY.TTF", 15, DB.Font[3]) -- 叠层：字体路径,数字是大小，后面是阴影
+	--B.SetFontSize(button.count, fontSize)
 
 	button.timer = button:CreateFontString(nil, "ARTWORK")
-	button.timer:SetPoint("TOP", button, "BOTTOM", 1, 2)
-	B.SetFontSize(button.timer, fontSize)
+	button.timer:SetPoint("TOP", button, "BOTTOM", 1, 4)
+	button.timer:SetFont("Interface\\AddOns\\NDui\\Media\\ROADWAY.TTF", 18, DB.Font[3]) -- 时间：字体路径,数字是大小，后面是阴影
+	--B.SetFontSize(button.timer, fontSize)
 
 	button.highlight = button:CreateTexture(nil, "HIGHLIGHT")
 	button.highlight:SetColorTexture(1, 1, 1, .25)
