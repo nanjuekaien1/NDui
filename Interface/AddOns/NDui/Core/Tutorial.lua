89af1f9ac3b1e636f35166aec279f302e25a3655
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-09-18 13:39:25 +0800

    Update Tutorial.lua

--- Interface/AddOns/NDui/Core/Tutorial.lua
+++ Interface/AddOns/NDui/Core/Tutorial.lua
@@ -40,7 +40,7 @@ local function ForceDefaultSettings()
 	if DB.isDeveloper then
 		SetCVar("ffxGlow", 0)
 		SetCVar("WorldTextScale", 1.2)
-		SetCVar("SpellQueueWindow", 160)
+		SetCVar("SpellQueueWindow", 100)
 		--SetCVar("AutoPushSpellToActionBar", 0)
 		--SetCVar("missingTransmogSourceInItemTooltips", 1)
 	end
