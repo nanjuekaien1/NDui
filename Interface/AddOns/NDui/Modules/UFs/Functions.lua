f711bacae1093982f17310dfad359e8c067a6cc9
siweia <siwei1990@outlook.com>
2026-09-23 04:35:47 +0800

    No desaturated on player

--- Interface/AddOns/NDui/Modules/UFs/Functions.lua
+++ Interface/AddOns/NDui/Modules/UFs/Functions.lua
@@ -1238,7 +1238,7 @@ local UNITFRAME_DESATURATED_DEBUFF_GROUPS = {
 }
 local UNITFRAME_PERSONAL_DEBUFF_LIMIT = 8
 local UNITFRAME_DESATURATED_DEBUFF_VALUES = {
-	Player = true,
+	--Player = true,
 	Target = true,
 	Focus = true,
 }
